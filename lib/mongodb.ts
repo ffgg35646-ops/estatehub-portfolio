9cf9ce5ef48ac9c7474b3a30c4d2e64a0cee8d4d
mohamed ali <ffgg35646@gmail.com>
2026-07-16 10:37:33 -0700

    mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,8 +4,17 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
-    "Please define MONGODB_URI inside .env.local"
+    "Please define the MONGODB_URI environment variable"
   );
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
 }
 
 let cached = global.mongoose;
@@ -18,14 +27,12 @@ if (!cached) {
 }
 
 async function connectDB() {
-  if (cached.conn) {
+  if (cached?.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose.connect(MONGODB_URI);
   }
 
   cached.conn = await cached.promise;
